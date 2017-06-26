21f916e2c3a756b93620e25b69319845d018c22a
wangning <346786495@qq.com>
2017-06-26 16:49:50 +0800

    add js part of navbar

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by wangning on 2017/6/26.
+ */
+
+/* js version of navbar pull down*/
+// var div_nodes = document.getElementsByClassName("m-title")
+// for(let div_node of div_nodes)
+// {
+//     div_node.onmouseover = function (e) {
+//         pullDown(e.currentTarget)
+//     }
+//     div_node.onmouseleave = function(e){
+//         pullBack(e.currentTarget)
+//     }
+// }
+//
+// function pullDown(target) {
+//     let img_node = target.getElementsByTagName("img")[0]
+//     img_node.style.cssText = "-moz-transform:scaleY(-1); -webkit-transform:scaleY(-1); -o-transform:scaleY(-1);transform:scaleY(-1);"
+//     let ul_node = target.getElementsByTagName("ul")[0]
+//     ul_node.style.zIndex = 999
+// }
+// function pullBack(target) {
+//     let img_node = target.getElementsByTagName("img")[0]
+//     img_node.style.cssText = ""
+//     let ul_node = target.getElementsByTagName("ul")[0]
+//     ul_node.style.zIndex = -999
+// }
+
