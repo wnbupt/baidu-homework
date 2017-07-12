710e1da5c3615808e90b9ad4c679f4e642f6fd3e
wangning <346786495@qq.com>
2017-07-12 11:43:57 +0800

    add table mission

--- baidu-homework-three/js/table.js
+++ baidu-homework-three/js/table.js
@@ -1,4 +1,5 @@
 var flag = true
+var isEditMode = true
 window.onload = function () {
     // add left list
     var obj = document.getElementsByClassName("g-li")[0]
@@ -41,6 +42,103 @@ window.onload = function () {
                 e.target.nextSibling.className = "sub-ul"
         }
     })
+
+
+    readTextFile("../baidu-homework-three/data/table.json", function(text){
+        var data = JSON.parse(text);
+        var tbody_node = document.getElementsByTagName("tbody")[0]
+        for(data_obj of data) {
+            var tr = document.createElement("tr")
+            tr.setAttribute("data-id",data_obj.id)
+            var td = document.createElement("td")
+            td.innerHTML = data_obj.name
+            tr.appendChild(td)
+            td = document.createElement("td")
+            td.innerHTML = data_obj.content
+            tr.appendChild(td)
+            td = document.createElement("td")
+            td.innerHTML = data_obj.value
+            tr.appendChild(td)
+            td = document.createElement("td")
+            var btn = document.createElement("button")
+            btn.innerHTML = "编辑"
+            btn.className = "edit-btn"
+            td.appendChild(btn)
+            btn = document.createElement("button")
+            btn.innerHTML = "删除"
+            btn.className = "del-btn"
+            td.appendChild(btn)
+            tr.appendChild(td)
+            tbody_node.append(tr)
+        }
+    })
+
+    var tbody_node = document.getElementsByTagName("tbody")[0]
+    // 获取父节点，并为它添加一个click事件
+    tbody_node.addEventListener("click",function(e) {
+        // 检查事件源e.targe是否为Li
+        if(e.target && e.target.nodeName.toUpperCase() == "BUTTON") {
+            if (e.target.className == "edit-btn") {
+                isEditMode = true
+                var id = e.target.parentNode.parentNode.getAttribute('data-id')
+                var name = e.target.parentNode.previousSibling.previousSibling.previousSibling.innerHTML
+                var content = e.target.parentNode.previousSibling.previousSibling.innerHTML
+                var value = e.target.parentNode.previousSibling.innerHTML
+                document.getElementsByClassName("u-fm")[0].innerHTML = '' +
+                  '<div class="u-fm f-cb"> ' +
+                  '<span>Name</span> ' +
+                  '<input type="text" class="tb-name">' +
+                  ' </div> ' +
+                  '<div class="u-fm f-cb"> ' +
+                  '<span>Content</span> ' +
+                  '<input type="text" class="tb-content"> ' +
+                  '</div> ' +
+                  '<div class="u-fm f-cb">' +
+                  '<span>Value</span>' +
+                  '<input type="text" class="tb-value">' +
+                  '</div>'
+
+                document.getElementsByClassName("g-hide")[0].setAttribute("data-id", id)
+                document.getElementsByClassName("tb-name")[0].value = name
+                document.getElementsByClassName("tb-content")[0].value = content
+                document.getElementsByClassName("tb-value")[0].value = value
+                document.getElementsByClassName("g-hide")[0].className = "g-hide active"
+
+            }
+            else if (e.target.className == "del-btn") {
+                isEditMode = false
+                var id = e.target.parentNode.parentNode.getAttribute('data-id')
+                var name = e.target.parentNode.previousSibling.previousSibling.previousSibling.innerHTML
+                document.getElementsByClassName("u-fm")[0].innerHTML = '<p class="tb-del-name">' + '确认删除' + name + '么？' + '</p>'
+                document.getElementsByClassName("g-hide")[0].setAttribute("data-id", id)
+                document.getElementsByClassName("g-hide")[0].className = "g-hide active"
+            }
+
+        }
+    })
+
+    document.getElementsByClassName("tb-submit")[0].onclick = function () {
+        if(isEditMode)
+        {
+            var id = document.getElementsByClassName("active")[0].getAttribute("data-id")
+            var name = document.getElementsByClassName("tb-name")[0].value
+            var content = document.getElementsByClassName("tb-content")[0].value
+            var value = document.getElementsByClassName("tb-value")[0].value
+            changeSubmitRow(id, name, content, value)
+            document.getElementsByClassName("active")[0].className = "g-hide"
+        }
+        else
+        {
+            var id = document.getElementsByClassName("active")[0].getAttribute("data-id")
+            RemoveSubmitRow(id)
+            document.getElementsByClassName("active")[0].className = "g-hide"
+        }
+    }
+
+    document.getElementsByClassName("tb-close")[0].onclick = function () {
+        document.getElementsByClassName("active")[0].className = "g-hide"
+    }
+
 }
 
 window.onscroll = function () {
@@ -85,6 +183,7 @@ function setScrollHeight() {
     document.getElementsByClassName("g-li")[0].style.height = h + 'px'
 }
 
+// read json file
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -97,3 +196,22 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
+function changeSubmitRow(id, name, content, value) {
+    var tbody_node = document.getElementsByTagName("tbody")[0]
+    var tr_node = tbody_node.getElementsByTagName("tr")[id-1]
+    tr_node.childNodes[0].innerHTML = name
+    tr_node.childNodes[1].innerHTML = content
+    tr_node.childNodes[2].innerHTML = value
+}
+
+function RemoveSubmitRow(id) {
+    var tbody_node = document.getElementsByTagName("tbody")[0]
+    var tr_node = tbody_node.getElementsByTagName("tr")
+    for(var i = 0; i < tr_node.length; i++){
+        if(tr_node[i].getAttribute("data-id") == id)
+        {
+            tbody_node.removeChild(tr_node[i])
+            break
+        }
+    }
+}
